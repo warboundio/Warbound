c4214b52f992622042542caec5427af39fd824f3
warbound.io <accounts@warbound.io>
2025-07-22 06:45:50 -0400

    Update VendorItem.cs

--- Data/Addon/VendorItem.cs
+++ Data/Addon/VendorItem.cs
@@ -6,12 +6,10 @@ namespace Data.Addon;
 [Table("g_vendor_item", Schema = "wow")]
 public class VendorItem
 {
-    [Key]
-    public int Id { get; set; }
-    public int ItemId { get; set; }
+    public int NPCId { get; set; }
+    public int VendorId { get; set; }
     public int Quantity { get; set; }
     public int Cost { get; set; }
     [MaxLength(7)] public string CostType { get; set; } = string.Empty;
     public int CostId { get; set; }
-    public int VendorId { get; set; }
 }
